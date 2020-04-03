a7b854c94b3b462265b1b8954f9c6433a7ea8f45
Artem <erlementovich@yandex.ru>
2020-04-03 12:46:52 +0300

    fix wishlist

--- web/app/themes/sage/resources/views/woocommerce/single-product/product-image.blade.php
+++ web/app/themes/sage/resources/views/woocommerce/single-product/product-image.blade.php
@@ -35,6 +35,7 @@ $wrapper_classes = apply_filters('woocommerce_single_product_image_gallery_class
 ?>
 <div class="<?php echo esc_attr(implode(' ', array_map('sanitize_html_class', $wrapper_classes))); ?>"
      data-columns="<?php echo esc_attr($columns); ?>" style="opacity: 0; transition: opacity .25s ease-in-out;">
+    @php echo do_shortcode('[yith_wcwl_add_to_wishlist]') @endphp
     <figure class="woocommerce-product-gallery__wrapper">
         <?php
         if ($product->get_image_id()) {
