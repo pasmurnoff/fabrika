30772979502d81a927ef387b3f50f74b86318005
Artem <erlementovich@yandex.ru>
2020-04-07 15:19:53 +0300

    fix checkout form

--- web/app/themes/sage/resources/functions/checkout.php
+++ web/app/themes/sage/resources/functions/checkout.php
@@ -12,3 +12,20 @@ function custom__billing_after_wrapper()
 
 add_action('woocommerce_before_checkout_billing_form', 'custom_billing_before_wrapper');
 add_action('woocommerce_after_checkout_billing_form', 'custom__billing_after_wrapper');
+
+add_filter('woocommerce_checkout_fields', 'addBootstrapToCheckoutFields');
+
+function addBootstrapToCheckoutFields($fields)
+{
+    foreach ($fields as &$fieldset) {
+        foreach ($fieldset as &$field) {
+            // if you want to add the form-group class around the label and the input
+            /* $field['class'][] = 'labelwrap';*/
+
+            // add form-control to the actual input
+            $field['input_class'][] = 'input';
+            $field['label_class'] = array('checkout__label');
+        }
+    }
+    return $fields;
+}
