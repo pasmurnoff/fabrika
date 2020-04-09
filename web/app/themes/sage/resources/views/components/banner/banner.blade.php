c0368c9d76a9b2c03c7a71b3b3b3f0badecec5e4
Artem <erlementovich@yandex.ru>
2020-04-09 17:00:22 +0300

    fix about page banner && title

--- web/app/themes/sage/resources/views/components/banner/banner.blade.php
+++ web/app/themes/sage/resources/views/components/banner/banner.blade.php
@@ -7,7 +7,14 @@
                     <div class="banner__text">{{ $text or '' }}</div>
                 @endisset
             </div>
-            <a href="{{ $href or '#'}}" class="button banner__button">{{ $buttonText or '' }}</a>
+            @if(isset($href2))
+                <div class="banner__btn-wrap">
+                    <a href="{{ $href or '#'}}" class="button banner__button">{{ $buttonText or '' }}</a>
+                    <a href="{{ $href2 }}" class="button banner__button">{{ $buttonText2 or '' }}</a>
+                </div>
+            @else
+                <a href="{{ $href or '#'}}" class="button banner__button">{{ $buttonText or '' }}</a>
+            @endif
         </div>
         <div class="banner__side banner__side_right">
             <div class="banner__icon">{{ $icon or ''}}</div>
