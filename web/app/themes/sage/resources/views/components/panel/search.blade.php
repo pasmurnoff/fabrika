335b6c75ab25604880b199153936db01ec4327b9
Artem <erlementovich@yandex.ru>
2020-04-10 16:03:35 +0300

    add main search

---
+++ web/app/themes/sage/resources/views/components/panel/search.blade.php
@@ -0,0 +1,5 @@
+<div class="panel__item panel__item_search">
+    <a class="panel__search panel__link callpopup" data-popup="main-search" href="#">
+        <i class="panel__icon">@include('icon::panel.search')</i>
+    </a>
+</div>
--- web/app/themes/sage/resources/views/components/panel/wrap.blade.php
+++ web/app/themes/sage/resources/views/components/panel/wrap.blade.php
@@ -1,5 +1,6 @@
 <div class="panel">
     <div class="panel__items">
+        @include('components.panel.search')
         @include('components.panel.wishlist')
         @include('components.panel.account')
         @include('components.panel.cart')
