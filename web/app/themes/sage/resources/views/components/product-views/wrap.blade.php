89c191f3196a4097d5f304f6dfc2fc57f65297ab
Artem <erlementovich@yandex.ru>
2020-04-17 16:58:50 +0300

    fix views output

--- web/app/themes/sage/resources/views/components/product-views/wrap.blade.php
+++ web/app/themes/sage/resources/views/components/product-views/wrap.blade.php
@@ -1,5 +1,11 @@
-<div class="product__quantity mrgn15-bottom">
-    <div class="text text_black">Просмотров за сегодня:
-        <b class="text text_bold text_grey">@php global $product; echo get_post_meta($product->get_id(), 'views', true) @endphp</b>
+@php
+    global $product;
+    $views = get_post_meta($product->get_id(), 'views', true) @endphp
+
+@if($views > 0)
+    <div class="product__quantity mrgn15-bottom">
+        <div class="text text_black">Просмотров за сегодня:
+            <b class="text text_bold text_grey">@php  echo $views @endphp</b>
+        </div>
     </div>
-</div>
+@endif
