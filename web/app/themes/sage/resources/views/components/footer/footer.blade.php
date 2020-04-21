0509f00c4c28584f96479a8a134e35a11d1a53ec
Artem <erlementovich@yandex.ru>
2020-04-21 20:17:08 +0300

    Add footer__header-block

--- web/app/themes/sage/resources/views/components/footer/footer.blade.php
+++ web/app/themes/sage/resources/views/components/footer/footer.blade.php
@@ -1,4 +1,5 @@
 <footer class="footer">
+    @include('components.footer.top.blocks.overtop')
     <div class="container">
         @include('components.footer.top.top')
         @include('components.footer.bottom.bottom')
