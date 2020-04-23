56494ed578d0254cdeca2844758bbdc7e732afbe
Artem <erlementovich@yandex.ru>
2020-04-23 16:16:17 +0300

    fix sticky-cart && phone icon

--- web/app/themes/sage/resources/assets/scripts/components/sticky-cart.js
+++ web/app/themes/sage/resources/assets/scripts/components/sticky-cart.js
@@ -1,8 +1,10 @@
 export default function sticky() {
-    const wrapper = $('.single-product');
+    const wrapper = $('.single-product'),
+        stickyPhone = $('.mswipe__trigger');
 
     function stickyCart() {
         $(window).scrollTop() >= $('.single_variation_wrap').offset().top ? wrapper.addClass('sticky_active') : wrapper.removeClass('sticky_active');
+        wrapper.hasClass('sticky_active') ? stickyPhone.css({bottom: $('.sticky-cart').height() + 15}) : stickyPhone.css({bottom: ''});
     }
 
     stickyCart();
