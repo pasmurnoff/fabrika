8fe756c53ce28287f8b2c64766998304b977afb5
andrejbarbasin <gmate@mail.ru>
2020-06-12 10:55:13 +0300

    Fix SEO cities

--- web/app/themes/sage/resources/views/layouts/app.blade.php
+++ web/app/themes/sage/resources/views/layouts/app.blade.php
@@ -3,47 +3,52 @@
 @include('components.head')
 <body {{ body_class('preload') }}>
 <div class="wrap">
-    @php do_action('get_header') @endphp
-    @include('components.header.wrap')
-    @php get_template_part('functions/watched') @endphp
+  @php do_action('get_header') @endphp
+  @include('components.header.wrap')
+  @php get_template_part('functions/watched') @endphp
 
-    <div class="container">
+  <div class="container">
 
-        @if( is_front_page() )
-            @php $title = 'Носки и колготки оптом. Продажа и изготовление на заказ.'; @endphp
-        @elseif(!is_product())
-            @php $title =  App::title(); @endphp
-        @endif
-
-        @if (isset($title))
-            @include('components.title.title', ['classes' => 'title_huge title_main', 'title' => $title])
-        @endif
-        @php
-            if (!is_front_page() && !is_product()) {
-              if (function_exists('yoast_breadcrumb')) {
-                yoast_breadcrumb('<p class="breadcrumbs">', '</p>');
-              } else {
-                echo 'no';
-              }
-            } elseif(is_product()) {
-                function_exists('yoast_breadcrumb') ? yoast_breadcrumb('<p class="breadcrumbs breadcrumbs_mrgn15-top">', '</p>') : '';
-            }
-        @endphp
-
-
-        <div class="content">
-            @yield('content')
-        </div>
-    </div>
-    @if(is_dynamic_sidebar('cart-sidebar'))
-        @php dynamic_sidebar( 'cart-sidebar' ); @endphp
+    @if( is_front_page() )
+      @php $title = 'Носки и колготки оптом. Продажа и изготовление на заказ.'; @endphp
+    @elseif(!is_product())
+      @php $title =  App::title(); @endphp
     @endif
-    @php do_action('get_footer') @endphp
 
-    @include('components.footer.footer')
+    @if(is_singular('city'))
+
+      @php $title = 'Носки оптом в ' . get_field('city-rod') . ' – Фабрика носков в ' . get_field('city-rod'); @endphp
+    @endif
+
+    @if (isset($title))
+      @include('components.title.title', ['classes' => 'title_huge title_main', 'title' => $title])
+    @endif
+    @php
+      if (!is_front_page() && !is_product()) {
+        if (function_exists('yoast_breadcrumb')) {
+          yoast_breadcrumb('<p class="breadcrumbs">', '</p>');
+        } else {
+          echo 'no';
+        }
+      } elseif(is_product()) {
+          function_exists('yoast_breadcrumb') ? yoast_breadcrumb('<p class="breadcrumbs breadcrumbs_mrgn15-top">', '</p>') : '';
+      }
+    @endphp
 
 
-    @php wp_footer() @endphp
+    <div class="content">
+      @yield('content')
+    </div>
+  </div>
+  @if(is_dynamic_sidebar('cart-sidebar'))
+    @php dynamic_sidebar( 'cart-sidebar' ); @endphp
+  @endif
+  @php do_action('get_footer') @endphp
+
+  @include('components.footer.footer')
+
+
+  @php wp_footer() @endphp
 </div>
 <div class="overlay"></div>
 </body>
