d3702c8677bd98bbb38968601e95ca0e640d0691
erlementovich <erlement@yandex.ru>
2020-07-06 13:10:00 +0300

    add table-size

--- web/app/themes/sage/resources/functions/new-custom-tabs.php
+++ web/app/themes/sage/resources/functions/new-custom-tabs.php
@@ -1,5 +1,5 @@
 <?php
-/*add_filter('woocommerce_product_tabs', 'woo_new_product_tab');*/
+add_filter('woocommerce_product_tabs', 'woo_new_product_tab');
 function woo_new_product_tab($tabs)
 {
     $tabs['product_sizes'] = array(
@@ -14,18 +14,6 @@ function woo_new_product_tab($tabs)
 
 function woo_product_sizes_table_output()
 {
-    echo tableProductSizes();
-}
-
-// Remove Empty Tabs
-add_filter('woocommerce_product_tabs', 'yikes_woo_remove_empty_tabs', 20, 1);
-
-function yikes_woo_remove_empty_tabs($tabs)
-{
-
-    if (!tableProductSizes())
-        unset($tabs['product_sizes']);
-
-    return $tabs;
+    tableProductSizes();
 }
 
