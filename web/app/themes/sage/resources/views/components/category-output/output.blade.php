f253712fdb667cb8a0f601b132e229e54be8205a
erlementovich <erlement@yandex.ru>
2020-07-07 11:51:07 +0300

    fix wishlist bug

--- web/app/themes/sage/resources/views/components/category-output/output.blade.php
+++ web/app/themes/sage/resources/views/components/category-output/output.blade.php
@@ -1,15 +1,38 @@
-{{--Выводим только родительские категории, если необходимо все остальные, то убираем   'parent' => 0 --}}
 @php
-        $productCategories = get_terms([
-            'taxonomy' => "product_cat",
-            'orderby' => 'count',
-        ])
+    $productCategories = get_terms([
+        'taxonomy' => "product_cat",
+        'orderby' => 'count',
+    ]);
+$publishedProductIds = [];
 @endphp
+
 @foreach($productCategories as $prodCat)
-    <div class="category-output mrgn35-top">
-        @include('components.category-output.title')
-        @include('components.category-output.list', ['overflow' => ''])
-        @include('components.page-index-banners.banners')
-    </div>
+    @php
+        $products = wc_get_products([
+            "product_cat" => $prodCat->slug,
+            "posts_per_page" => isset($count) ? $count : 12,
+            'post_status' => 'publish',
+            'exclude' => $publishedProductIds
+            ])
+    @endphp
+    @if($products)
+        <div class="category-output mrgn35-top">
+            @include('components.category-output.title')
+            <ul class="category-output__list products @php echo isset($overflow) ? 'products_overflow' : ''  @endphp">
+                @foreach ($products as $product)
+                    @php
+                        $inStock = $product->is_in_stock();
+                    @endphp
+                    @include('components.product.product')
+                    @php $publishedProductIds[] = $product->get_id() @endphp
+                @endforeach
+            </ul>
+
+            @include('components.page-index-banners.banners')
+        </div>
+    @endif
 @endforeach
+
+
+
 
--- web/app/themes/sage/resources/views/page-index.blade.php
+++ web/app/themes/sage/resources/views/page-index.blade.php
@@ -4,8 +4,8 @@
 @extends('layouts.app')
 
 @section('content')
-{{--    @include('components.tags.wrap')--}}
-{{--    @include('components.card.stories.wrap')--}}
+    {{--    @include('components.tags.wrap')--}}
+    {{--    @include('components.card.stories.wrap')--}}
     @include('components.banner.banner-duo')
     @include('components.category-output.output')
     @include('components.content.page-index-content')
