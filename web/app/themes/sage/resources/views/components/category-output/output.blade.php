558c5511f8348fc5752eccc5b7244f8972a57c09
erlementovich <erlement@yandex.ru>
2020-07-10 13:52:00 +0300

    fix page-index products output && page-optom products output

--- web/app/themes/sage/resources/views/components/category-output/output.blade.php
+++ web/app/themes/sage/resources/views/components/category-output/output.blade.php
@@ -1,8 +1,20 @@
 @php
+    $parentCategories = get_terms([
+        'taxonomy' => "product_cat",
+        'orderby' => 'count',
+        'parent' => 0
+    ]);
+
+    $parentIds = [];
+    foreach ($parentCategories as $parent)
+        $parentIds[] = $parent->term_id;
+
     $productCategories = get_terms([
         'taxonomy' => "product_cat",
         'orderby' => 'count',
+        'exclude' => $parentIds
     ]);
+
 $publishedProductIds = [];
 @endphp
 
@@ -13,9 +25,16 @@ $publishedProductIds = [];
             "posts_per_page" => isset($count) ? $count : 12,
             'post_status' => 'publish',
             'exclude' => $publishedProductIds
-            ])
+            ]);
+
+    $productCount = count($products);
+
+        if(($productCount > 6) && $productCount < 12) {
+            $products = array_slice($products, 0 , 6, true);
+        }
+
     @endphp
-    @if($products)
+    @if($products && (count($products) >= 6))
         <div class="category-output mrgn35-top">
             @include('components.category-output.title')
             <ul class="category-output__list products @php echo isset($overflow) ? 'products_overflow' : ''  @endphp">
