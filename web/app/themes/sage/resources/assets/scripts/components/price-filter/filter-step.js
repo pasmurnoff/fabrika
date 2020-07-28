3018ce0f641cc9d773ff690b31f015dd3d5cb580
erlementovich <BycsorucAvyith1>
2020-07-28 12:07:27 +0300

    fix slider

--- web/app/themes/sage/resources/assets/scripts/components/price-filter/filter-step.js
+++
@@ -1,8 +0,0 @@
-export default function filterStep(min, max, step) {
-    const count = (max - min) / step + 1;
-    let i = 1;
-    while (i <= count) {
-        $('.price-filter__values').append('<div class="price-filter__value">' + i * step + '</div>')
-        i++
-    }
-}
--- web/app/themes/sage/resources/views/components/price-filter/wrap.blade.php
+++ web/app/themes/sage/resources/views/components/price-filter/wrap.blade.php
@@ -2,19 +2,28 @@
     <h3 class="title title_middle mrgn35-bottom">Количество</h3>
     <div class="price-filter__wrapper">
         <div class="price-filter__per-one text mrgn15-bottom">
-            <span class="price-filter__old-price"><del><span>40</span> ₽&nbsp;/&nbsp;За одну пару</del></span>
+            <span class="price-filter__old-price"><del><span id="maxValue" data-max="{{ get_field('max') }}">{{ get_field('max') }}</span> ₽&nbsp;/&nbsp;За одну пару</del></span>
             <span><span id="perOne">20</span> ₽&nbsp;/&nbsp;За одну пару</span>
         </div>
         <div class="price-filter__slider-wrap">
             <div class="price-filter__content">
-                <input type="range" min="1000" max="5000" step="1000" class="price-filter__input" id="priceFilter"
+                <input type="range" min="1000" max="4000" step="1000" class="price-filter__input" id="priceFilter"
                        value="2000"
-                       data-price="20">
+                       <?php $i = 1 ?>
+                       <?php foreach (get_field('values') as $value) :?>
+                       data-value{{$i}}={{$value['quantity']}}
+                               data-price{{$i++}}={{$value['price']}}
+                <?php endforeach; ?>
+                >
                 <div class="price-filter__lower-fill"></div>
                 <div class="price-filter__upper-fill"></div>
                 <div class="price-filter__count text" id="priceDemo"></div>
             </div>
             <div class="price-filter__values">
+                @foreach(get_field('values') as $value)
+                    <div class="price-filter__value">{{ $value['quantity'] }}</div>
+                    <div class="price-filter__price hidden">{{ $value['price'] }}</div>
+                @endforeach
             </div>
         </div>
     </div>
