a2233c00c64f839f7f7ae3628cfbd97ceb33b79f
Nikita Malkin <demeja16@gmail.com>
2021-02-03 17:11:36 +0300

    try to fix site crush in single product + trello tasks

--- web/app/themes/sage/resources/views/components/category-output/all/title.blade.php
+++ web/app/themes/sage/resources/views/components/category-output/all/title.blade.php
@@ -1,19 +1,23 @@
-<div class="category-output__title-wrap mrgn35 pdng15-bottom {{ $class or '' }}">
-    <h2 class="title title_large category-output_title">@php echo $prodCat->name @endphp</h2>
-    @php $parentArr = ['detskoe', 'zhenskoe', 'muzhskoe'] @endphp
+@if ($prodCat && !is_wp_error($prodCat))
+  <div class="category-output__title-wrap mrgn35 pdng15-bottom {{ $class or '' }}">
+    <h2 class="title title_large category-output_title"{!! $prodCat->name !!}</h2>
+
+    @php 
+      $parentArr = [
+        'detskoe', 
+        'zhenskoe', 
+        'muzhskoe',
+      ];
+    @endphp
+
     @if(!in_array($prodCat->slug, $parentArr))
-        {{--<a href="@php echo get_term_link($prodCat) @endphp"
-           class="button button_transparent">Вся @php echo mb_strtolower($prodCat->name) @endphp </a>--}}
-        <a href="@php echo get_term_link($prodCat) @endphp"
-           class="button button_transparent">{{ $prodCat->name }} оптом</a>
+      <a href="{!! get_term_link($prodCat) !!}" class="button button_transparent">{{ $prodCat->name }} оптом</a>
     @elseif($prodCat->slug == 'detskoe')
-        <a href="@php echo get_term_link($prodCat) @endphp"
-           class="button button_transparent">Открыть детскую коллекцию</a>
+      <a href="{!! get_term_link($prodCat) !!}" class="button button_transparent">Открыть детскую коллекцию</a>
     @elseif($prodCat->slug == 'zhenskoe')
-        <a href="@php echo get_term_link($prodCat) @endphp"
-           class="button button_transparent">Открыть женскую коллекцию</a>
+      <a href="{!! get_term_link($prodCat) !!}" class="button button_transparent">Открыть женскую коллекцию</a>
     @elseif($prodCat->slug == 'muzhskoe')
-        <a href="@php echo get_term_link($prodCat) @endphp"
-           class="button button_transparent">Открыть мужскую коллекцию</a>
+      <a href="{!! get_term_link($prodCat) !!}" class="button button_transparent">Открыть мужскую коллекцию</a>
     @endif
-</div>
+  </div>
+@endif
--- web/app/themes/sage/resources/views/components/product/colors.blade.php
+++ web/app/themes/sage/resources/views/components/product/colors.blade.php
@@ -1,16 +1,22 @@
 @php
     $variation_data = wc_get_product($productId)->get_attributes();
-    $variation_colors = $variation_data['pa_cvet'];
-    $color_ids = $variation_colors['options'];
+    $variation_colors = null;
+    $color_ids = null;
+
+    if(isset($variation_data['pa_cvet'])) {
+        $variation_colors = $variation_data['pa_cvet'];
+        $color_ids = $variation_colors['options'];
+    }
 @endphp
 
-@if($color_ids)
-    <div class="product__colors filter__colors">
+
+<div class="product__colors filter__colors">
+    @if($color_ids)
         @foreach($color_ids as $id)
             @php $color = get_term_meta($id, 'color', 'true'); @endphp
             <div class="product__color-container filter__item">
                 <div class="product__color filter__color" style="background-color:{{$color}}"></div>
             </div>
         @endforeach
-    </div>
-@endif
+    @endif
+</div>
