c94c76af27d180b76077a25e2def7474b89fc6fa
malikov-n <malikov.kzn@gmail.com>
2021-03-29 11:22:14 +0300

    пагинация с фильтром цветов

--- web/app/themes/sage/resources/views/components/filter/filter-color.blade.php
+++ web/app/themes/sage/resources/views/components/filter/filter-color.blade.php
@@ -1,8 +1,10 @@
 @php
     $terms = get_terms('pa_cvet');
-        $url = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
-        $constUrl = $url;
-
+        //$url = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+        //$constUrl = $url;
+    $category = get_queried_object();
+    $category_id = $category->term_id;
+    $url = get_category_link( $category_id );
 @endphp
 <section class="mrgn35-top">
     <h3 class="title title_middle">Цвета</h3>
@@ -16,7 +18,7 @@
             @endphp
             @if($color)
                 @php
-                    $currentColor = strpos($constUrl, $slug) ? 'filter__item_current' : '';
+                    $currentColor = strpos($url, $slug) ? 'filter__item_current' : '';
                     $url = add_query_arg(['filter_cvet' => $slug], $url);
                 @endphp
 
