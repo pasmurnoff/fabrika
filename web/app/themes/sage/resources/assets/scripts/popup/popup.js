059bef291aa964f589bffa92b6f2fa8b763a4921
malikov-n <malikov.kzn@gmail.com>
2021-03-30 10:16:12 +0300

    бесящее всплывающее модальное окно

--- web/app/themes/sage/resources/assets/scripts/popup/popup.js
+++ web/app/themes/sage/resources/assets/scripts/popup/popup.js
@@ -1,12 +1,12 @@
 jQuery(document).ready(function () {
-    /* Show support-modal popup */
+    /* Show modal popup */
     $('.callpopup').on('click', function () {
         const block = $(this).data('popup');
         $('.' + block).addClass('active');
         $('.overlay').addClass('active');
     });
 
-    /* Hide support-modal popup */
+    /* Hide modal popup */
     $('.closepopup').on('click', function () {
         const block = $(this).data('popup');
         $('.' + block).removeClass('active');
--- web/app/themes/sage/resources/views/components/footer/top/blocks/support.blade.php
+++ web/app/themes/sage/resources/views/components/footer/top/blocks/support.blade.php
@@ -1,2 +1,15 @@
 <a class="support-btn callpopup" data-popup="support-modal">Служба поддержки</a>
 @include('components.support-modal.support-modal')
+
+<hr>
+Footer:
++ 1. Служба поддержки, изменить крестик, добавить анимацию появления и исчезания
++ 2. Отцентровать
+
+Страница товара:
++ 1. Кнопка нашли дешевле, на мобилках крестик не работает
+
+https://www.fabrikanoskov.ru/konstruktor-zakaza-noskov/
+Поправить NaN
+
++ Убрать это (бесящая модалка)
