24c6a493fcfe84851f235f6dc496c8f8531ed2e1
malikov-n <malikov.kzn@gmail.com>
2021-04-06 17:07:33 +0300

    gallery popup

--- web/app/themes/sage/resources/assets/scripts/routes/singleProduct.js
+++ web/app/themes/sage/resources/assets/scripts/routes/singleProduct.js
@@ -1,12 +1,14 @@
 /* Sticky cart for product */
 import sticky from '../components/sticky-cart';
 import toggleShare from '../components/share';
+import galleryPopup from '../gallery-popup/init';
 import generateProductPdf from '../components/generate-product-pdf/init';
 
 export default {
     init() {
         sticky();
         toggleShare();
+        galleryPopup()
         generateProductPdf();
     },
     finalize() {
