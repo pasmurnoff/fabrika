3cadb8ae82e596760124a53cb5fd2e4364051e27
malikov-n <malikov.kzn@gmail.com>
2021-04-08 11:45:46 +0300

    включить перевод и верхнее меню в bem_menu

--- web/app/themes/sage/resources/views/components/header/menu.blade.php
+++ web/app/themes/sage/resources/views/components/header/menu.blade.php
@@ -1,8 +1,4 @@
-<ul class="menu menu_inline">
-  <li class="menu__item menu__item_top"><a class="menu__link menu__link_grey menu__link_top" href="/noski-optom">Носки оптом</a></li>
-  <li class="menu__item menu__item_top"><a class="menu__link menu__link_grey menu__link_top" href="/o-fabrike">О фабрике</a></li>
-  <li class="menu__item menu__item_top"><a class="menu__link menu__link_grey menu__link_top" href="/punkty-vydachi">Пункты выдачи</a></li>
-  <li class="menu__item menu__item_top"><a class="menu__link menu__link_grey menu__link_top" href="/etiketka-i-upakovka-noskov">Этикетка и упаковка носков</a></li>
-  <li class="menu__item menu__item_top"><a class="menu__link menu__link_grey menu__link_top" href="/pomosh">Задать вопрос</a></li>
-  <li class="menu__item menu__item_top menu__item_bold">Бесплатная консультация: @include('common.phone', ['class' => 'phone_bold phone_top'])</li>
-</ul>
+<div class="menu-top__wrap">
+  {!! bem_menu('header_top','menu-top') !!}
+  <div class="menu-top__item menu-top__bold">Бесплатная консультация: @include('common.phone', ['class' => 'menu-top__link'])</div>
+</div>
