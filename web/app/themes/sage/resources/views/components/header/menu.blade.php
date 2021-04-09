28e8abda54a85c4c87e5a2a24f4fc0c2edbbb975
malikov-n <malikov.kzn@gmail.com>
2021-04-09 11:08:07 +0300

    страница о фабрике

--- web/app/themes/sage/resources/views/components/header/menu.blade.php
+++ web/app/themes/sage/resources/views/components/header/menu.blade.php
@@ -1,4 +1,4 @@
 <div class="menu-top__wrap">
   {!! bem_menu('header_top','menu-top') !!}
-  <div class="menu-top__item menu-top__bold">Бесплатная консультация: @include('common.phone', ['class' => 'menu-top__link'])</div>
+  <div class="menu-top__item menu-top__bold">{{ __('Бесплатная консультация', 'sage') }}:&nbsp;@include('common.phone', ['class' => 'menu-top__link'])</div>
 </div>
