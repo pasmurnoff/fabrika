26fb0a28db9452a1e28f4d2abae332c2566b85c0
malikov-n <malikov.kzn@gmail.com>
2021-04-13 11:42:27 +0300

    Как выбрать размер

--- web/app/themes/sage/resources/views/page-vakancy.blade.php
+++ web/app/themes/sage/resources/views/page-vakancy.blade.php
@@ -9,30 +9,11 @@
             <img class="defaultpage__image" src="@asset('images/defaultpages/vakancii_fabrika_noskov.jpg')"
                  alt="Вакансии на фабрику носков" itemprop="contentUrl">
         </div>
-        <div class="block-text">
-            <div class="block-text__text">
-                «Фабрика носков» всегда ищет высококлассных специалистов в свою команду по следующим направлениям:
+
+        @while(have_posts()) @php the_post() @endphp
+            <div class="the-content">
+                @php the_content() @endphp
             </div>
-            <ul class="block-text__list">
-                <li class="block-text__point">Специалист по продажам</li>
-                <li class="block-text__point">Водитель</li>
-                <li class="block-text__point">Бухгалтер</li>
-                <li class="block-text__point">Дизайнер</li>
-                <li class="block-text__point">Грузчик</li>
-                <li class="block-text__point">Логист</li>
-                <li class="block-text__point">Специалист отдела закупок</li>
-                <li class="block-text__point">Менеджер по продажам</li>
-                <li class="block-text__point">Региональный менеджер</li>
-                <li class="block-text__point">Директор по продажам</li>
-                <li class="block-text__point">Креативщик/Рекламщик</li>
-            </ul>
-            <div class="block-text__text">
-                Официальное трудоустройство<br/>
-                Официальная заработная плата<br/>
-                Полис добровольного страхования<br/>
-                Оплата сотовой связи<br/>
-                Свое резюме в электронном виде, просим присылать на нашу электронную почту mail@fabrikanoskov.ru
-            </div>
-        </div>
+        @endwhile
     </div>
 @endsection
