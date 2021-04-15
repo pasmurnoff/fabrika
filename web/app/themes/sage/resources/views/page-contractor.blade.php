f761ef55f1491a5cf7769dec1df671ef09178dd4
malikov-n <malikov.kzn@gmail.com>
2021-04-15 18:03:07 +0300

    15.04

--- web/app/themes/sage/resources/views/page-contractor.blade.php
+++ web/app/themes/sage/resources/views/page-contractor.blade.php
@@ -9,30 +9,12 @@
       <img class="defaultpage__image" src="@asset('images/defaultpages/fabrikanoskov_stat-podryadchikom.jpg')"
            alt="Стать подрядчиком фабрики носков">
     </div>
-    <div class="block-text">
-      <div class="block-text__text">
-        Стать нашим подрядчиком.
-      </div><br/>
-      <div class="block-text__text block-text__text_bold">
-        Вы можете стать нашими подрядчиками при соответствии нижеуказанным условиям:
-      </div>
-      <ul class="block-text__list">
-        <li class="block-text__point">Ваша организация обладает мощностями по производству носочно-чулочных изделий более, чем 1 млн. пар в год;</li>
-        <li class="block-text__point">Продукция выпускаемая вашим производством сертифицирована и подготовлена к реализации;</li>
-        <li class="block-text__point">Номенклатура товаров состоит из более, чем 100 позиций во всех популярных размерах и цветах;</li>
-        <li class="block-text__point">Производство и отправка продукции до конечного клиента должна осуществляться силами подрядчика;</li>
-        <li class="block-text__point">Соблюдение договорных сроков производства и поставки готовой продукции;</li>
-        <li class="block-text__point">Возможность производства изделий с уникальным дизайном;</li>
-        <li class="block-text__point">Оборудование должно иметь возможность использовать не менее 6 цветов при производстве готовой носочно-чулочной продукции;</li>
-        <li class="block-text__point">Организации существует не менее 3 лет;</li>
-        <li class="block-text__point">Штат не менее 18 сотрудников производственного направления;</li>
-        <li class="block-text__point">Обязательное наличие дизайнера в штате;</li>
-        <li class="block-text__point">Возможность отпускать оптовые (от 100 пар) и розничные тиражи (от 1 пары) ;</li>
-        <li class="block-text__point">Производство должно находиться на территории Российской Федерации;</li>
-        <li class="block-text__point">Каждое изделие должно иметь уникальную фотографию, описание и цену (розничную и оптовую) ;</li>
-        <li class="block-text__point">Прогрессивная система лояльности;</li>
-      </ul>
 
-    </div>
+    @while(have_posts()) @php the_post() @endphp
+      <div class="the-content">
+        @php the_content() @endphp
+      </div>
+    @endwhile
+
   </div>
 @endsection
--- web/app/themes/sage/resources/views/page-rent.blade.php
+++ web/app/themes/sage/resources/views/page-rent.blade.php
@@ -10,34 +10,11 @@ Template Name: Предложить помещение в аренду
            alt="Предложить помещение в аренду">
     </div>
 
-    <div class="block-text">
-      <div class="block-text__text block-text__text_padding-away">
-        «Фабрика носков» ищет собственников помещений, готовых заключить арендный договор на срок более 11 месяцев.
+    @while(have_posts()) @php the_post() @endphp
+      <div class="the-content">
+        @php the_content() @endphp
       </div>
-    </div>
-
-    <div class="block-text">
-      <div class="block-text__text block-text__text_bold">
-        Арендные площади должны соответствовать следующим критериям:
-      </div>
-      <ul class="block-text__list">
-        <li class="block-text__point">Площадь коммерческой недвижимости, не менее 20 квадратных метров (до 10 квадратных метров внутри ТРЦ, в формате остров)</li>
-        <li class="block-text__point">Обязательное наличие складского помещения (от 5 квадратных метров)</li>
-        <li class="block-text__point">Обязательное наличие туалета</li>
-        <li class="block-text__point">Отдельная входная группа</li>
-        <li class="block-text__point">Плотный пешеходный трафик</li>
-        <li class="block-text__point">Возможность установки вывесочного оборудования</li>
-        <li class="block-text__point">Витражные окна будут считаться преимуществом</li>
-        <li class="block-text__point">Белый цвет помещений будет считаться преимуществом</li>
-        <li class="block-text__point">Арендные каникулы на время ремонта будет считаться преимуществом</li>
-        <li class="block-text__point">Уверенное покрытие сотовых сетей</li>
-        <li class="block-text__point">Наличие проводного интернета (не менее 20 М/бит)</li>
-        <li class="block-text__point">Минимальное удаление от офисов транспортных компаний будет считаться преимуществом</li>
-        <li class="block-text__point">Наличие автомобильного трафика будет считаться преимуществом</li>
-        <li class="block-text__point">Городское население, не менее 150000 человек</li>
-        <li class="block-text__point">Наличии муниципальной парковки будет считаться преимуществом</li>
-      </ul>
-    </div>
+    @endwhile
   </div>
 
   @include('components.writeus.writeus')
