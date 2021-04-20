04314c7a86230f127738f11dd7d81ace444daafe
malikov-n <malikov.kzn@gmail.com>
2021-04-20 17:53:11 +0300

    fix form

--- web/app/themes/sage/resources/assets/scripts/forms/ajax.js
+++ web/app/themes/sage/resources/assets/scripts/forms/ajax.js
@@ -6,32 +6,34 @@ function dynamicAnimate(elem) {
 }
 
 $(document).ready(function () {
-    $('form.form').validate({
-        errorClass: 'form-error',
-        ...formRules,
-        submitHandler: function (form) {
-            let formData = new FormData(form);
-            $.ajax({
-                contentType: false, // важно - убираем форматирование данных по умолчанию
-                processData: false, // важно - убираем преобразование строк по умолчанию
-                url: window.wp_data.ajax_url + '?action=send_mail',
-                type: 'POST', //метод отправки
-                data: formData,  // Сеарилизуем объект
-                success: function () { //Данные отправлены успешно
-                    let $resultForm = $('<div class="result-message text text_success">Ваше сообщение было успешно отправлено.\n' +
-                        'Mы свяжемся с Вами в ближайшее время.</div>').hide();
-                    $('body').append($resultForm);
-                    dynamicAnimate($resultForm);
-                },
-                error: function () { // Данные не отправлены
-                    let $resultForm = $('<div class="result-message text text_dangerous">Что-то пошло не так, попробуйте в другой раз</div>').hide();
-                    $('body').append($resultForm);
-                    dynamicAnimate($resultForm);
-                },
-            });
+    $('.form').each((i, item) => {
+        $(item).validate({
+            errorClass: 'form-error',
+            ...formRules,
+            submitHandler: function (form) {
+                let formData = new FormData(form);
+                $.ajax({
+                    contentType: false, // важно - убираем форматирование данных по умолчанию
+                    processData: false, // важно - убираем преобразование строк по умолчанию
+                    url: window.wp_data.ajax_url + '?action=send_mail',
+                    type: 'POST', //метод отправки
+                    data: formData,  // Сеарилизуем объект
+                    success: function () { //Данные отправлены успешно
+                        let $resultForm = $('<div class="result-message text text_success">Ваше сообщение было успешно отправлено.\n' +
+                            'Mы свяжемся с Вами в ближайшее время.</div>').hide();
+                        $('body').append($resultForm);
+                        dynamicAnimate($resultForm);
+                    },
+                    error: function () { // Данные не отправлены
+                        let $resultForm = $('<div class="result-message text text_dangerous">Что-то пошло не так, попробуйте в другой раз</div>').hide();
+                        $('body').append($resultForm);
+                        dynamicAnimate($resultForm);
+                    },
+                });
 
 
-            return false;
-        },
-    });
+                return false;
+            },
+        });
+    })
 });
