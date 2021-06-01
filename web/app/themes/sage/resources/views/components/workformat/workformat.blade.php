c2c6c3ce819e2042834f881e0a66350799e5d17b
malikov-n <malikov.kzn@gmail.com>
2021-06-01 17:15:45 +0300

    перевод шаблона "Менеджер по продажам"

--- web/app/themes/sage/resources/views/components/workformat/workformat.blade.php
+++ web/app/themes/sage/resources/views/components/workformat/workformat.blade.php
@@ -1,7 +1,10 @@
-<div class="workformat mrgn35">
-  <div class="title title_huge workformat__title">Выберите подходящий формат работы</div>
-  <div class="workformat__text">
-    Разъездная: станьте лицом компании Фабрика носков в Вашем городе.<br>
-    Из дома: работайте из любого региона России в удобное время.
+@php
+  $manager_format_title = get_field('manager_format_title');
+  $manager_format_text = get_field('manager_format_text');
+@endphp
+@if($manager_format_title)
+  <div class="workformat mrgn35">
+    <div class="title title_huge workformat__title">{!! $manager_format_title !!}</div>
+    <div class="workformat__text">{!! $manager_format_text !!}</div>
   </div>
-</div>
+@endif
--- web/app/themes/sage/resources/views/components/worksteps/worksteps.blade.php
+++ web/app/themes/sage/resources/views/components/worksteps/worksteps.blade.php
@@ -1,21 +1,23 @@
-<div class="worksteps mrgn35">
-  <div class="title title_huge worksteps__title">Четыре шага до работы</div>
-  <div class="worksteps__inner">
-    <div class="worksteps__item">
-      @include('icon::worksteps.step1')
-      <div class="worksteps__text">Зарегистрируйтесь и станьте оптовым менеджером</div>
-    </div>
-    <div class="worksteps__item">
-      @include('icon::worksteps.step2')
-      <div class="worksteps__text">Пройдите собеседование по телефону</div>
-    </div>
-    <div class="worksteps__item">
-      @include('icon::worksteps.step3')
-      <div class="worksteps__text">Закончите онлайн обучение</div>
-    </div>
-    <div class="worksteps__item">
-      @include('icon::worksteps.step4')
-      <div class="worksteps__text">Пройдите практику и приступайте к работе</div>
-    </div>
+@php
+  $manager_steps_title = get_field('manager_steps_title');
+  $manager_steps = get_field('manager_steps');
+@endphp
+@if(!empty($manager_steps_title))
+  <div class="worksteps mrgn35">
+    <div class="title title_huge worksteps__title">{!! $manager_steps_title !!}</div>
+    @if($manager_steps)
+      <div class="worksteps__inner">
+        @foreach($manager_steps as $step)
+          @if($loop->index > 3)
+            {{-- допускаем не более 4 шагов --}}
+            @break
+          @endif
+          <div class="worksteps__item">
+            @include('icon::worksteps.step' . ($loop->index + 1) )
+            <div class="worksteps__text">{!! $step['step'] !!}</div>
+          </div>
+        @endforeach
+      </div>
+    @endif
   </div>
-</div>
+@endif
