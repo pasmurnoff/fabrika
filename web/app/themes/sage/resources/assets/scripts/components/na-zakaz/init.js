22a4b1ddfd6c40ba2359d18ee35db824dae0fc74
artem <artem@startted.com>
2021-06-16 11:23:00 +0300

    fix slider

--- web/app/themes/sage/resources/assets/scripts/components/na-zakaz/init.js
+++ web/app/themes/sage/resources/assets/scripts/components/na-zakaz/init.js
@@ -1,18 +1,24 @@
 export default function () {
     if ($(window).width() < 768) {
-        $('.block-order__wrapper').slick({
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            arrows: false,
-            asNavFor: '.block-order__nav',
-            autoplay: true,
-        });
-        $('.block-order__nav').slick({
-            slidesToShow: 4,
-            slidesToScroll: 4,
-            asNavFor: '.block-order__wrapper',
-            centerMode: true,
-            focusOnSelect: true,
-        });
+        const blockOrderWrapper = $('.block-order__wrapper'),
+            blockOrderNav = $('.block-order__nav');
+
+        if (blockOrderWrapper.length)
+            blockOrderWrapper.slick({
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                arrows: false,
+                asNavFor: '.block-order__nav',
+                autoplay: true,
+            });
+
+        if (blockOrderNav.length)
+            blockOrderNav.slick({
+                slidesToShow: 4,
+                slidesToScroll: 4,
+                asNavFor: '.block-order__wrapper',
+                centerMode: true,
+                focusOnSelect: true,
+            });
     }
 }
