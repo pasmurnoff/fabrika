255eeedd10b4d841a2405a1625b0035626456d9b
Malikov <malikov.kzn@gmail.com>
2021-06-16 20:27:39 +0300

    сертификаты

--- web/app/themes/sage/resources/views/components/gallery/files.blade.php
+++ web/app/themes/sage/resources/views/components/gallery/files.blade.php
@@ -1,12 +1,13 @@
 @if(!empty($files))
-    <pre>
-        <?php
-                print_r($files);
-        ?>
-    </pre>
-
-    <div class="files-gallery">
-
+    <div class="files-gallery {{ $class or '' }}">
+        @foreach($files as $item)
+            @if(!empty($item['img']) and !empty($item['file']))
+                <div class="files-gallery__item">
+                    <a href="{{ $item['file']['url'] }}" class="files-gallery__item-link" target="_blank">
+                        <img src="{{ $item['img']['sizes']['medium'] }}" class="files-gallery__item-img" alt="{!! $item['img']['alt'] !!}">
+                    </a>
+                </div>
+            @endif
+        @endforeach
     </div>
-
 @endif
--- web/app/themes/sage/resources/views/page-sertifikaty.blade.php
+++ web/app/themes/sage/resources/views/page-sertifikaty.blade.php
@@ -4,15 +4,11 @@ Template Name: Сертификаты
 
 @extends('layouts.app')
 @section('content')
-    @while(have_posts()) @php the_post() @endphp
 
-        @include('components.thumbnail.wrap')
+    @include('components.thumbnail.wrap')
 
-        <div class="the-content">
-            @php the_content() @endphp
-        </div>
+    @include('components.gallery.files', ['files' => get_field( 'files_gallery', get_the_ID() ), 'class' => 'mrgn35-bottom'])
 
-        @include('components.gallery.files', ['files' => get_field( 'files_gallery', get_the_ID() )])
+    @include('components.writeus.writeus')
 
-    @endwhile
 @endsection
