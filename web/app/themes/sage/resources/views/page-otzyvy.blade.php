555dd73fd61036f40ddee55d4c0607641b70afbb
Malikov <nail.malikov@startted.com>
2021-06-18 16:59:06 +0300

    review microdata schema

--- web/app/themes/sage/resources/views/page-otzyvy.blade.php
+++ web/app/themes/sage/resources/views/page-otzyvy.blade.php
@@ -9,25 +9,6 @@ Template Name: Отзывы
 
     @include('components.form.review', ['title' => 'Добавить отзыв'])
 
-
-{{--    @php do_action('generateRVW') @endphp--}}
-
-{{--    <form action method="post" enctype="multipart/form-data">--}}
-{{--        <input type="hidden" name="addReview">--}}
-
-{{--        <input type="text" name="review_author"> <br><br>--}}
-{{--        <textarea name="review_body"></textarea> <br><br>--}}
-{{--        <input type="text" name="review_rating"> <br><br>--}}
-{{--        <input type="file" name="review_image" multiple="false"> <br><br>--}}
-{{--        <button class="button">Добавить отзыв</button>--}}
-{{--    </form>--}}
-
-{{--    <pre>--}}
-{{--        <?php--}}
-{{--        print_r($_SERVER['HTTP_REFERER']);--}}
-{{--        ?>--}}
-{{--    </pre>--}}
-
     @include('components.reviews.wrap')
 
 @endsection
